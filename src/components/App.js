2f31e2f5f44509d19e4bf4efecc41b23edaf6218
Shreyas <121279572+Shreyasssonar@users.noreply.github.com>
2023-12-13 04:14:00 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,19 @@
-
-import React from "react";
 import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  let count =[count, setCount ] = useState(0);
+  const [count, setCount] = useState(0);
 
-  const increment () =>{
-    setCount(count+1);
+  const increment = () => {
+    setCount(count + 1);
   };
+
   return (
     <div>
-        <p>Button clicked {count} times</p>
-        <button onClick={increment}>click me</button>
+      <p>Button clicked {count} times</p>
+      <button onClick={increment}>click me</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
